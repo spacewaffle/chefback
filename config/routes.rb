74c9c98e5ab5512901ecff1fb3973ee4b57297c8
Jon Cheng <jon.syndicate@gmail.com>
2013-06-01 19:27:21 -0400

    added some styles

--- config/routes.rb
+++ config/routes.rb
@@ -59,7 +59,11 @@ Chefback::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-   root :to => 'registrations#new'
+   devise_scope :user do
+    root :to => 'registrations#new'
+  end
+    root :to => 'ingredients#new'
+   
 
   # See how all your routes lay out with "rake routes"
 
