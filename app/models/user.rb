5d544b077dbf8169cf8f5f66a2de52faa5ae4f58
Stanley Chiang <stanchiang@outlook.com>
2013-06-01 19:57:42 -0400

    good models and forms

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,19 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
+
+  
+  has_many :foods
+  has_many :ingredients
+  has_many :inventories
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
+
   attr_accessible :email, :password, :remember_me, :stripe_card_token
 
   # attr_accessible :title, :body
+
 end
