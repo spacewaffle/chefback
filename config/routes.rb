1542e84885ee55aebbd58a487667ade79b85f0c8
Jon Cheng <jon.syndicate@gmail.com>
2013-06-02 01:15:38 -0400

    fixed header and routes file to work with active admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Chefback::Application.routes.draw do
-  devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
+  devise_scope :user do
+    #root :to => 'registrations#new'
+  end
 
+  root :to => 'ingredients#new'
   resources :ingredients
 
 
@@ -13,8 +15,8 @@ Chefback::Application.routes.draw do
 
 
   devise_for :users, :controllers => { :registrations => "registrations", :sessions => "sessions" }
+  devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  resources :users
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -64,10 +66,7 @@ Chefback::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-   devise_scope :user do
-    root :to => 'registrations#new'
-  end
-    root :to => 'ingredients#new'
+
    
 
   # See how all your routes lay out with "rake routes"
