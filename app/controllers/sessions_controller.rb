6c265b6ad682887214de71a8c2ebc5e58f11de19
Jon Cheng <jon.syndicate@gmail.com>
2013-06-01 22:58:24 -0400

    fixed sign in and sign out routes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,9 @@
 class SessionsController < Devise::SessionsController
-   protected
-      def after_sign_in_path_for(resource)
-        current_user
-      end          
+protected
+  def after_sign_in_path_for(resource)
+    inventories_path
+  end
+  def after_sign_out_path_for(resource_or_scope)
+    new_user_session_path
+  end
 end
